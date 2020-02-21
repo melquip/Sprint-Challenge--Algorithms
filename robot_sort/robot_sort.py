6dc52dccc07f3520fcafe7a6d80eb22cf1ea08be
Melquisedeque Pereira <melqui1@live.com.pt>
2020-02-21 10:37:22 +0000

    Completed First pass `robot_sort` solution

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -55,9 +55,11 @@ class SortingRobot:
         of it.
         This will increment the time counter by 1.
         """
+        # print(f'-- {self._item} {self._list[self._position]} --')
         self._time += 1
         # Swap the held item with the list item at the robot's position
         self._item, self._list[self._position] = self._list[self._position], self._item
+        # print(f'-- {self._item} {self._list[self._position]} --')
 
     def compare_item(self):
         """
@@ -96,17 +98,74 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        # while swaps keep happening
+        while not self.light_is_on():
+          # set the light on
+          self.set_light_on()
+          # while robot can move right
+          while self.can_move_right():
+            # if its not holding any item or at an invalid position
+            if self.compare_item() == None:
+              # pick up an item from the list
+              self.swap_item()
+              # move forward
+              self.move_right()
+            # If the held item's value is greater than item in front of robot
+            if self.compare_item() == 1:
+              # leave bigger value here and take lower value
+              self.swap_item()
+              # move backwards
+              self.move_left()
+              # leave lower value behind
+              self.swap_item()
+              # move forward
+              self.move_right()
+              # did a swap so light off and continue swapping
+              self.set_light_off()
+            # If the held item's value is less than item in front of robot
+            if self.compare_item() == -1:
+              # move backwards
+              self.move_left()
+              # leave lower value here but pickup "None"
+              self.swap_item()
+              # move forward
+              self.move_right()
+            # If the held item's value is equal to item in front of robot
+            if self.compare_item() == 0:
+              # move backwards
+              self.move_left()
+              # leave equal value here but pickup "None"
+              self.swap_item()
+              # move forward
+              self.move_right()
+
+          while self.can_move_left() and not self.light_is_on():
+            self.move_left()
+        print('end?', self._list)
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
+    l = [1, -38, -95, 4, 23, -73, -65, -36, 85, 2, 58, -26, -55, 96, 55, -76, 64, 45, 69, 36, 69, 47, 29, -47, 13, 89, -57, -88, -87, 54, 60, 56, -98, -78, 59, 93, -41, -74, 73, -35, -23, -79, -35, 46, -18, -18, 37, -64, 14, -57, -2, 15, -85, 45, -73, -2, 79, -87, -100, 21, -51, 22, 26, -59, 81, 59, -24, 24, -81, 43, 61, 52, 38, -88, -95, 87, -57, -37, -65, -47, -3, 21, -77, 98, 25, 1, -36, 39, 78, 47, -35, -40, -69, -81, 11, -47, 21, 25, -53, -31]
     robot = SortingRobot(l)
 
     robot.sort()
     print(robot._list)
+
+
+"""
+  for i in range(0, len(self._list)):
+    if self._item == None:
+      self._item = self._list[i]
+    self.goForward()
+    print('robot at', self._position, 'with', self._item)
+    for item in self._list[self._position:]:
+      print('found', item)
+      if self._item > item:
+        self.swap_item()
+        print('swap', self._position, self._item)
+      # else:
+        # self.goForward()
+"""
